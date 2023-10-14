b28558c020a3dc521de447a74be5ccd41276f55c
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-10-14 14:00:16 +0530

    added updated tf files

--- asg_old/user_data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-     sudo su
-     yum update -y
-     yum install httpd -y
-     systemctl start httpd
-     systemctl enable httpd
-     echo "<h1>Terraform Learning from $(hostname -f)..</h1>" > /var/www/html/index.html
