7b9e9ccb3ea2554e5e2f2d2d91960705cb4884c5
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-10-24 02:28:46 +0530

    new org anf workspace

--- terraform.tf
+++ terraform.tf
@@ -1,9 +1,9 @@
 terraform {
   cloud {
-    organization = "CG_Tokyo"
+    organization = "Satish_Terraform"
 
     workspaces {
-      name = "Terraform_POC"
+      name = "Terraform_Final"
     }
   }
 }
